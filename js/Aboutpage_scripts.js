8d321eba85aea004707c47c2c9335ab6d6fca0e5
Sagar Suman <sagarsuman1299@gmail.com>
2021-09-21 12:05:02 +0530

    website

---
+++ js/Aboutpage_scripts.js
@@ -0,0 +1,36 @@
+var li_elements = document.querySelectorAll(".about_wrapper_left ul li");
+var item_elements = document.querySelectorAll(".about_item");
+
+
+for (var i = 0; i < li_elements.length; i++) {
+
+  li_elements[i].addEventListener("click", function() 
+  {
+    
+    li_elements.forEach(function(li) {
+      li.classList.remove("active");
+    });
+    this.classList.add("active");
+
+    var li_value = this.getAttribute("data-li");
+
+    item_elements.forEach(function(item) {
+      item.style.display = "none";
+      
+    });
+
+    if (li_value == "about_how") {
+      document.querySelector("." + li_value).style.display = "block";
+      document.querySelector(".welcome").style.display = "none";
+    } else if (li_value == "about_what") {
+      document.querySelector("." + li_value).style.display = "block";
+      document.querySelector(".welcome").style.display = "none";
+    } else if (li_value == "about_team") {
+      document.querySelector("." + li_value).style.display = "block";
+      document.querySelector(".welcome").style.display = "none";
+     } 
+    else {
+      console.log("");
+    }
+  });
+}
